1d05b127d3293295ab8d33039b752a668c153b7d
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-08 20:27:26 +0700

    exam 1 done

--- src/main/kotlin/com/dicoding/exam/exam1/Exam1.kt
+++ src/main/kotlin/com/dicoding/exam/exam1/Exam1.kt
@@ -2,15 +2,15 @@ package com.dicoding.exam.exam1
 
 // TODO 1
 fun isEvenNumber(number: Int): Boolean {
-    return false
+    return if (number % 2 == 0) true else false
 }
 
 // TODO 2
 fun moreThanFive(number: Int): Boolean {
-    return false
+    return if (number > 5) true else false
 }
 
 // TODO 3
 fun result(number: Int): Int {
-    return 0
+    return number * (number + 10)
 }
