2669400a5ae1da057ad2be6ee31913a7e4845921
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-08 20:52:18 +0700

    exam 2 done

--- src/main/kotlin/com/dicoding/exam/exam2/Exam2.kt
+++ src/main/kotlin/com/dicoding/exam/exam2/Exam2.kt
@@ -2,10 +2,10 @@ package com.dicoding.exam.exam2
 
 // TODO 1
 fun calculate(valueA: Int, valueB: Int, valueC: Int?): Int {
-    return 0
+    return valueA + (valueB - (valueC ?: 50))
 }
 
 // TODO 2
 fun result(result: Int): String {
-    return ""
+    return "Result is $result"
 }
