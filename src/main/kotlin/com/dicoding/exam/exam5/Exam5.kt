6eb45f04a68e7ce0ee88f95c34fac95f1cee80fb
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-10 05:10:41 +0700

    exam 5 revision

--- src/main/kotlin/com/dicoding/exam/exam5/Exam5.kt
+++ src/main/kotlin/com/dicoding/exam/exam5/Exam5.kt
@@ -3,13 +3,13 @@ package com.dicoding.exam.exam5
 import kotlinx.coroutines.delay
 
 // TODO 1
-fun sum(valueA: Int, valueB: Int): Int {
-    suspend { delay(3000L) }
+suspend fun sum(valueA: Int, valueB: Int): Int {
+    delay(3000L)
     return valueA + valueB
 }
 
 // TODO 2
-fun multiple(valueA: Int, valueB: Int): Int {
-    suspend { delay(2000L) }
+suspend fun multiple(valueA: Int, valueB: Int): Int {
+    delay(2000L)
     return valueA * valueB
 }
