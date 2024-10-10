0823c3ebb6e9580ed8fa01e20ffa0cd53dff54a8
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-10 10:24:30 +0700

    optional exam 4 done

--- src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
@@ -2,5 +2,13 @@ package com.dicoding.exam.optionalexam4
 
 // TODO
 fun getMiddleCharacters(string: String): String {
-    return ""
+    var middleCharacter = ""
+    if (string.length <= 100) {
+        if (string.length % 2 == 0) {
+            middleCharacter = "${string[(string.length/2) - 1]}${string[string.length/2]}"
+        } else {
+            middleCharacter = "${string[string.length/2]}"
+        }
+    }
+    return middleCharacter
 }
