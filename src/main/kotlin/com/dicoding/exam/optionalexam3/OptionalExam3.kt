208e832cd33b80cbc68b2243f6de9db3fa70c941
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-11 10:22:47 +0700

    optional exam 3 done

--- src/main/kotlin/com/dicoding/exam/optionalexam3/OptionalExam3.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam3/OptionalExam3.kt
@@ -2,5 +2,15 @@ package com.dicoding.exam.optionalexam3
 
 // TODO
 fun manipulateString(str: String, int: Int): String {
-    return ""
+    var result = ""
+    val separatedString = Regex("""\d+|\D+""").findAll(str).map { it.groupValues.first() }.toList()
+    if (separatedString.count() > 1) {
+        val text = separatedString.first()
+        val number = separatedString.last().toInt()
+        val math = number * int
+        result = text + math.toString()
+    } else {
+        result = separatedString.first() + int.toString()
+    }
+    return result
 }
