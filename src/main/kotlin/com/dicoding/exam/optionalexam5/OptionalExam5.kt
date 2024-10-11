b2f4a3a7acf4a208a8a15dbb4041224d2ab1fe4e
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-11 08:59:33 +0700

    optional exam 5 revision

--- src/main/kotlin/com/dicoding/exam/optionalexam5/OptionalExam5.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam5/OptionalExam5.kt
@@ -1,6 +1,4 @@
 package com.dicoding.exam.optionalexam5
 
 // TODO
-fun concatString(string1: String, string2: String): String {
-    return ""
-}
+val concatString: (string1: String, string2: String) -> String = String::plus
