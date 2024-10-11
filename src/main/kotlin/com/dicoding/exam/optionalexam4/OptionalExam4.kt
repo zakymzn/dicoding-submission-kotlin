bfec50bedafeb36fc63a9c7b3dd391a7b85f1df5
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-11 10:26:31 +0700

    perbaikan lint

--- src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
@@ -5,9 +5,9 @@ fun getMiddleCharacters(string: String): String {
     var middleCharacter = ""
     if (string.length <= 100) {
         if (string.length % 2 == 0) {
-            middleCharacter = "${string[(string.length/2) - 1]}${string[string.length/2]}"
+            middleCharacter = "${string[(string.length / 2) - 1]}${string[string.length / 2]}"
         } else {
-            middleCharacter = "${string[string.length/2]}"
+            middleCharacter = "${string[string.length / 2]}"
         }
     }
     return middleCharacter
