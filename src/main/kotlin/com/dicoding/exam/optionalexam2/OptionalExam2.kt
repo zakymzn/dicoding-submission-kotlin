d4086aaaa07b1544189ce3f33947573fcded4bce
zakymzn <mamurzakynurrokhman@gmail.com>
2024-10-11 09:24:06 +0700

    optional exam 2 done

--- src/main/kotlin/com/dicoding/exam/optionalexam2/OptionalExam2.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam2/OptionalExam2.kt
@@ -2,5 +2,12 @@ package com.dicoding.exam.optionalexam2
 
 // TODO
 fun minAndMax(number: Int): Int {
-    return 0
+    val stringNumber = number.toString()
+    val numberList = stringNumber.toList()
+    val sortedNumber = numberList.sorted()
+    var result = 0
+    if (stringNumber.length in 2..9) {
+        result = sortedNumber.first().digitToInt() + sortedNumber.last().digitToInt()
+    }
+    return result
 }
